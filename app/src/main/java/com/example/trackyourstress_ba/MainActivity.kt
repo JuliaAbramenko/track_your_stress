5d93874385707cc1dd28681555dfeef9b6512ad5
Julia Abramenko <julia.abramenko@uni-ulm.de>
2019-11-05 16:23:44 +0100

    rebuild from scratch

---
+++ app/src/main/java/com/example/trackyourstress_ba/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trackyourstress_ba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
