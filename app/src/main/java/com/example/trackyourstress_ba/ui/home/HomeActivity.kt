3c30d8269b5d84459252ec890b5164706f2fbb51
Julia Abramenko <julia.abramenko@uni-ulm.de>
2019-11-10 21:37:02 +0100

    - added navigation drawer (ongoing)

--- app/src/main/java/com/example/trackyourstress_ba/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/trackyourstress_ba/ui/home/HomeActivity.kt
@@ -4,26 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.example.trackyourstress_ba.R
 import com.example.trackyourstress_ba.kotlin.GlobalVariables
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var yourtoken: TextView
-    lateinit var youremail: TextView
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        yourtoken = findViewById(R.id.yourtoken)
-        youremail = findViewById(R.id.youremail)
 
     }
 
     override fun onStart() {
         super.onStart()
-        yourtoken.text = GlobalVariables.localStorage["token"]
-        youremail.text = GlobalVariables.localStorage["current_email"]
+        toolbar = findViewById(R.id.toolbar)
+
     }
 
 
