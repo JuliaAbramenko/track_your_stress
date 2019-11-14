d5476ae52629fbc588019567ade522d7ec83a8c3
Julia Abramenko <julia.abramenko@uni-ulm.de>
2019-11-14 21:51:19 +0100

    - added navigation drawer behaviour (open/close) - fixed actionbar problem

--- app/src/main/java/com/example/trackyourstress_ba/kotlin/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.trackyourstress_ba.kotlin
-
-class User(_email: String, _password: String, _password_confirmation: String, _username: String) {
-    val email = _email
-    val password = _password
-    val password_confirm = _password_confirmation
-    val username = _username
-}
--- app/src/main/java/com/example/trackyourstress_ba/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/trackyourstress_ba/ui/home/HomeActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.trackyourstress_ba.R
 import com.example.trackyourstress_ba.kotlin.GlobalVariables
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    lateinit var toolbar: Toolbar
+    lateinit var drawer : DrawerLayout
+    lateinit var drawer_toggle : ActionBarDrawerToggle
+    lateinit var nav_view : NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +28,22 @@ class HomeActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawer = findViewById(R.id.drawer_layout)
+        drawer_toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(drawer_toggle)
+        drawer_toggle.syncState()
+        nav_view = findViewById(R.id.nav_view)
+        nav_view.setItemIconTintList(null)
 
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }
+        else super.onBackPressed()
     }
 
 
